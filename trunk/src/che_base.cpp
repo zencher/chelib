659a8f03f31dab9b3fb25237b2c363c92b7b7819
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2010-06-21 14:49:03 +0000

    注释掉基类CHE_Object的内存分配和释放操作函数，待内存管理的具体方案和细节的确认
    git-svn-id: https://10.211.55.3/svn/chelib@20 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/src/che_base.cpp
+++ trunk/src/che_base.cpp
@@ -2,23 +2,23 @@
 #include "../include/che_base.h"
 
 
-HE_VOID* CHE_Object::operator new( size_t size )
-{
-	return malloc( size );
-}
-
-HE_VOID CHE_Object::operator delete( HE_VOID* p )
-{
-	free( p );
-}
-
-HE_VOID* CHE_Object::operator new( size_t size, HE_LPCSTR lpszFileName, HE_INT32 nLine )
-{
-	return malloc( size );
-}
-
-
-HE_VOID CHE_Object::operator delete( HE_VOID* p, HE_LPCSTR lpszFileName, HE_INT32 nLine )
-{
-	free( p );
-}
+// HE_VOID* CHE_Object::operator new( size_t size )
+// {
+// 	return malloc( size );
+// }
+// 
+// HE_VOID CHE_Object::operator delete( HE_VOID* p )
+// {
+// 	free( p );
+// }
+// 
+// HE_VOID* CHE_Object::operator new( size_t size, HE_LPCSTR lpszFileName, HE_INT32 nLine )
+// {
+// 	return malloc( size );
+// }
+// 
+// 
+// HE_VOID CHE_Object::operator delete( HE_VOID* p, HE_LPCSTR lpszFileName, HE_INT32 nLine )
+// {
+// 	free( p );
+// }

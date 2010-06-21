714c4874f516561386564e599fce18b22d9faad0
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2010-06-21 14:46:45 +0000

    移除基本数据类型的宏定义
    注释掉基类 CHE_Object 的内存分配和释放操作，等待后续内存管理模块的细节确认
    
    git-svn-id: https://10.211.55.3/svn/chelib@18 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/che_base.h
+++ trunk/include/che_base.h
@@ -1,126 +1,18 @@
 #ifndef _CHE_BASE_H_
 #define _CHE_BASE_H_
 
-#include <stddef.h>
-
-#pragma pack(2)
-
-typedef void	HE_VOID;
-
-//Pointer to any type.
-typedef void *  HE_LPVOID;
- 
-//Pointer to any constant type. 
-typedef void const *  HE_LPCVOID;
-
-//Byte (8 bits).
-typedef unsigned char  HE_BYTE;
-
-//Pointer to a HE_BYTE.
-typedef unsigned char *  HE_LPBYTE;
-
-//Pointer to a constant HE_BYTE.
-typedef unsigned char const *  HE_LPCBYTE;
- 
-//Short integer (16 bits).
-typedef short  HE_SHORT;
- 
-//16-bit unsigned integer.
-typedef unsigned short  HE_WORD;
- 
-//Pointer to a DWORD.
-typedef unsigned short *  HE_LPWORD;
- 
-//32-bit signed integer.
-typedef int  HE_INT32;
-
-typedef unsigned int HE_UINT32;
- 
-//32-bit unsigned integer. 
-typedef unsigned long  HE_DWORD;
-
-//Pointer to a DWORD. 
-typedef unsigned long *  HE_LPDWORD;
-
-//Boolean variable (should be TRUE or FALSE).
-typedef int  HE_BOOL;
- 
-//8-bit Windows (ANSI) character. 
-typedef char  HE_CHAR;
-
-//Pointer to 8-bit Windows (ANSI) characters.
-typedef char *  HE_LPSTR;
- 
-//Pointer to constant 8-bit Windows (ANSI) characters. 
-typedef char const *  HE_LPCSTR;
-
-//16-bit Unicode character. 
-//typedef wchar_t  HE_WCHAR;
-
-//Pointer to 16-bit Unicode characters.
-//typedef wchar_t *  HE_LPWSTR;
- 
-//Pointer to constant 16-bit Unicode characters.
-//typedef wchar_t const *  HE_LPCWSTR;
- 
-//A HE_WCHAR if UNICODE is defined, a HE_CHAR otherwise.
-//typedef char  HE_TCHAR 
- 
-//A HE_LPWSTR if UNICODE is defined, a HE_LPSTR otherwise. 
-//typedef char *  HE_LPTSTR 
-
-//A HE_LPCWSTR if UNICODE is defined, a HE_LPCSTR otherwise.
-//typedef char const *  HE_LPCTSTR 
- 
-//A value used to denote the position of an element in a collection.
-//typedef void *  HE_POSITION 
-
-typedef long HE_LONG;
-
-//Signed 64-bit integer. 
-typedef long int HE_INT64;
-
-//Unsigned 64-bit integer.
-typedef unsigned long HE_UINT64;
-
-//Signed integral type for pointer precision.
-typedef int  HE_INTPTR;
-
-//Unsigned integral type for pointer precision.
-typedef unsigned int HE_UINTPTR;
-
-//32-bit floating-point number.
-typedef float  HE_FLOAT;
-
-typedef double HE_DOUBLE;
-
-#define FALSE 0
-#define TRUE 1
-#define NULL 0
-
-
-struct HE_RECT
-{
-	HE_DWORD left;
-	HE_DWORD top;
-	HE_DWORD width;
-	HE_DWORD height;
-};
-
-#define HE_ABS abs
+#include "che_define.h"
 
 class CHE_Object
 {
-public:
-	HE_LPVOID operator new( size_t );
-
-	HE_LPVOID operator new( size_t, HE_LPCSTR, HE_INT32 );
-
-	HE_VOID  operator delete( HE_LPVOID );
-
-	HE_VOID  operator delete( HE_LPVOID, HE_LPCSTR, HE_INT32 );
-
-
+// public:
+// 	HE_LPVOID operator new( size_t );
+// 
+// 	HE_LPVOID operator new( size_t, HE_LPCSTR, HE_INT32 );
+// 
+// 	HE_VOID  operator delete( HE_LPVOID );
+// 
+// 	HE_VOID  operator delete( HE_LPVOID, HE_LPCSTR, HE_INT32 );
 };
 
 #endif

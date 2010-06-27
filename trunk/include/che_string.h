7947b9c1e0c629b7fa3d344107ab00ac3f9b3c8a
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2010-06-27 11:52:18 +0000

    加入字符串相加操作的函数定义
    git-svn-id: https://10.211.55.3/svn/chelib@25 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/che_string.h
+++ trunk/include/che_string.h
@@ -36,19 +36,19 @@ public:
 												//的值，实际上，临时变量是const，根本不能被赋值。
 	//HE_CHAR& operator[]( HE_INT32 index );		//函数返回了对象保存的字符串的一个字符的引用，任何操作都会互相影响。不好！！！
 
-// 	//如下能够前后颠倒的操作，还需要在全局的空间里面定义全局的函数。
-// 	const CHE_ByteString& operator+( const CHE_ByteString& str );
-// 	const CHE_ByteString& operator+( HE_CHAR ch );
-// 	const CHE_ByteString& operator+( const HE_LPSTR lpStr );
-// 	//以上三个函数应该返回一个const对象引用，因为上面三个函数的操作，在语义上是返回一个临时对象，对临时对象赋值是错误的
+	//如下能够前后颠倒的操作，还需要在全局的空间里面定义全局的函数。
+	CHE_ByteString operator+( const CHE_ByteString& str );
+	CHE_ByteString operator+( HE_CHAR ch );
+	CHE_ByteString operator+( const HE_LPSTR lpStr );
+	//以上三个函数的操作，在语义上是返回一个临时对象，返回一个不是const的临时对象，可以支持连续操作+的使用。
 
-// 	friend const CHE_ByteString& operator+( HE_CHAR ch, const CHE_ByteString& str );
-// 	friend const CHE_ByteString& operator+( const HE_LPSTR lpStr, const CHE_ByteString& str );
+ 	friend CHE_ByteString operator+( HE_CHAR ch, const CHE_ByteString& str );
+ 	friend CHE_ByteString operator+( const HE_LPSTR lpStr, const CHE_ByteString& str );
 	
 	CHE_ByteString& operator+=( const CHE_ByteString& str );
 	CHE_ByteString& operator+=( HE_CHAR ch );
 	CHE_ByteString& operator+=( const HE_LPSTR str );
-	//以上三个函数不应该返回一个const对象的引用，因为这三个函数对对象本身进行了操作，所以返回的是本身，这样可以支持连续的操作符使用
+	//以上三个函数不应该返回一个const对象的引用(这个引用时对象本身的引用)，因为这三个函数对对象本身进行了操作，所以返回的是本身，这样可以支持连续的操作符使用
 	
 	bool operator!=( const CHE_ByteString& str );
 	bool operator!=( HE_CHAR ch );
@@ -57,14 +57,6 @@ public:
 	friend bool operator!=( HE_CHAR ch, const CHE_ByteString& str );
 	friend bool operator!=( const HE_LPSTR lpStr, const CHE_ByteString& str );
 	
-	//bool operator>( const CHE_ByteString& str );
-	//bool operator>( HE_CHAR ch );
-	//bool operator>( const HE_LPSTR str );
-	
-	//bool operator<( const CHE_ByteString& str );
-	//bool operator<( HE_CHAR ch );
-	//bool operator<( const HE_LPSTR str );
-	
 private:
 	HE_StringData* m_lpData;
 };
@@ -72,8 +64,8 @@ private:
 bool operator==( HE_CHAR ch, const CHE_ByteString& str );
 bool operator==( HE_LPSTR lpStr, const CHE_ByteString& str );
 
-// const CHE_ByteString& operator+( HE_CHAR ch, const CHE_ByteString& str );
-// const CHE_ByteString& operator+( const HE_LPSTR lpStr, const CHE_ByteString& str );
+CHE_ByteString operator+( HE_CHAR ch, const CHE_ByteString& str );
+CHE_ByteString operator+( const HE_LPSTR lpStr, const CHE_ByteString& str );
 
 bool operator!=( HE_CHAR ch, const CHE_ByteString& str );
 bool operator!=( const HE_LPSTR lpStr, const CHE_ByteString& str );

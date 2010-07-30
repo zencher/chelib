a1f4250d62cfb8526c1f841206a5cdd5b6bc2044
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2010-07-30 02:53:23 +0000

    修改CHE_ByteString的数据结构体的名称为ByteStringData
    git-svn-id: https://10.211.55.3/svn/chelib@28 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/che_string.h
+++ trunk/include/che_string.h
@@ -3,7 +3,7 @@
 
 #include "che_base.h"
 
-struct HE_StringData
+struct HE_ByteStringData
 {
 	HE_LPSTR m_lpString;
 	HE_LONG m_dwLength;
@@ -25,14 +25,14 @@ public:
 	CHE_ByteString& operator=( HE_LPCSTR lpStr );	//同上
 	CHE_ByteString& operator=( const CHE_ByteString& str );
 
-	bool operator==( HE_CHAR ch );
-	bool operator==( HE_LPCSTR lpStr );
-	bool operator==( const CHE_ByteString& str );
+	bool operator==( HE_CHAR ch )const;
+	bool operator==( HE_LPCSTR lpStr )const;
+	bool operator==( const CHE_ByteString& str )const;
 
-	friend bool operator==( HE_CHAR ch, const CHE_ByteString& str );
-	friend bool operator==( HE_LPSTR lpStr, const CHE_ByteString& str );
+	friend bool operator==( HE_CHAR ch, CHE_ByteString& str );
+	friend bool operator==( HE_LPSTR lpStr, CHE_ByteString& str );
 
-	HE_CHAR operator[]( HE_INT32 index );		//函数以传值的方式返回一个临时的HE_CHAR，对HE_CHAR的赋值不会影响string内部
+	HE_CHAR operator[]( HE_INT32 index )const;		//函数以传值的方式返回一个临时的HE_CHAR，对HE_CHAR的赋值不会影响string内部
 												//的值，实际上，临时变量是const，根本不能被赋值。
 	//HE_CHAR& operator[]( HE_INT32 index );		//函数返回了对象保存的字符串的一个字符的引用，任何操作都会互相影响。不好！！！
 
@@ -42,32 +42,32 @@ public:
 	CHE_ByteString operator+( const HE_LPSTR lpStr );
 	//以上三个函数的操作，在语义上是返回一个临时对象，返回一个不是const的临时对象，可以支持连续操作+的使用。
 
- 	friend CHE_ByteString operator+( HE_CHAR ch, const CHE_ByteString& str );
- 	friend CHE_ByteString operator+( const HE_LPSTR lpStr, const CHE_ByteString& str );
+ 	friend CHE_ByteString operator+( HE_CHAR ch, CHE_ByteString& str );
+ 	friend CHE_ByteString operator+( const HE_LPSTR lpStr, CHE_ByteString& str );
 	
 	CHE_ByteString& operator+=( const CHE_ByteString& str );
 	CHE_ByteString& operator+=( HE_CHAR ch );
-	CHE_ByteString& operator+=( const HE_LPSTR str );
+	CHE_ByteString& operator+=( const HE_LPSTR lpStr );
 	//以上三个函数不应该返回一个const对象的引用(这个引用时对象本身的引用)，因为这三个函数对对象本身进行了操作，所以返回的是本身，这样可以支持连续的操作符使用
 	
-	bool operator!=( const CHE_ByteString& str );
-	bool operator!=( HE_CHAR ch );
-	bool operator!=( const HE_LPSTR str );
+	bool operator!=( const CHE_ByteString& str )const;
+	bool operator!=( HE_CHAR ch )const;
+	bool operator!=( const HE_LPSTR lpStr )const;
 
-	friend bool operator!=( HE_CHAR ch, const CHE_ByteString& str );
-	friend bool operator!=( const HE_LPSTR lpStr, const CHE_ByteString& str );
+	friend bool operator!=( HE_CHAR ch, CHE_ByteString& str );
+	friend bool operator!=( const HE_LPSTR lpStr, CHE_ByteString& str );
 	
 private:
-	HE_StringData* m_lpData;
+	HE_ByteStringData* m_lpData;
 };
 
-bool operator==( HE_CHAR ch, const CHE_ByteString& str );
-bool operator==( HE_LPSTR lpStr, const CHE_ByteString& str );
+bool operator==( HE_CHAR ch, CHE_ByteString& str );
+bool operator==( HE_LPSTR lpStr, CHE_ByteString& str );
 
-CHE_ByteString operator+( HE_CHAR ch, const CHE_ByteString& str );
-CHE_ByteString operator+( const HE_LPSTR lpStr, const CHE_ByteString& str );
+CHE_ByteString operator+( HE_CHAR ch, CHE_ByteString& str );
+CHE_ByteString operator+( const HE_LPSTR lpStr, CHE_ByteString& str );
 
-bool operator!=( HE_CHAR ch, const CHE_ByteString& str );
-bool operator!=( const HE_LPSTR lpStr, const CHE_ByteString& str );
+bool operator!=( HE_CHAR ch, CHE_ByteString& str );
+bool operator!=( const HE_LPSTR lpStr, CHE_ByteString& str );
 
 #endif

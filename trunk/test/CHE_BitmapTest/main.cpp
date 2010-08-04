987f9e011c053e965cd7240b3fc755476cc12d15
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2010-08-04 08:33:09 +0000

    git-svn-id: https://10.211.55.3/svn/chelib@37 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/test/CHE_BitmapTest/main.cpp
+++ trunk/test/CHE_BitmapTest/main.cpp
@@ -3,6 +3,85 @@
 
 int main()
 {
+	CHE_Bitmap aBitmap;
+	aBitmap.Create( 25, 25, BITMAP_FORMAT_32BPP, BITMAP_ORIG_BOTTOM );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_32BPP, BITMAP_ORIG_TOP );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_24BPP, BITMAP_ORIG_BOTTOM );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_24BPP, BITMAP_ORIG_TOP );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_8BPP, BITMAP_ORIG_BOTTOM );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_8BPP, BITMAP_ORIG_TOP );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_4BPP, BITMAP_ORIG_BOTTOM );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_4BPP, BITMAP_ORIG_TOP );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_1BPP, BITMAP_ORIG_BOTTOM );
+	//aBitmap.Create( 25, 25, BITMAP_FORMAT_1BPP, BITMAP_ORIG_TOP );
+	aBitmap.Fill( 0xFFFFFFFF );
+
+//	aBitmap.Save( "OriABitmap.bmp" );
+//	char tempFileName[1024];
+
+//	int iPixelCount = aBitmap.Width() * aBitmap.Height();
+//	for ( int i = 0; i < aBitmap.Height() + 10; i++ )	//增加10，检查超过位图范围的情况
+//	{
+//		for ( int j = 0; j < aBitmap.Width() + 10; j++ )	//增加10，检查超过位图范围的情况
+//		{
+//			aBitmap.SetPixel( j, i, 0x00FF0000 );	//Set different color
+//			sprintf( tempFileName, "SetPixBitmap(%04d-%04d).bmp", i, j );
+//			aBitmap.Save( tempFileName );
+//		}
+//	}
+
+// 	HE_RECT rt;
+// 	rt.top = 0;
+// 	rt.left = 0;
+// 	rt.height = aBitmap.Height();
+// 	rt.width = aBitmap.Width();
+// 	aBitmap.Fill( 0x00FF0000, &rt );
+// 	aBitmap.Save( "FillRectAll.bmp" );
+
+// 	for ( int i = 0; i < aBitmap.Width()+10; i++ )	//增加10，检查超过位图范围的情况
+// 	{
+// 		rt.width = i;
+// 		aBitmap.Fill( 0x000000FF, &rt );	//Set different color
+// 		sprintf( tempFileName, "FillRect(width - %04d).bmp", i );
+// 		aBitmap.Save( tempFileName );
+// 	}
+
+// 	for ( int j = 0; j < aBitmap.Width()+10; j++ )	//增加10，检查超过位图范围的情况
+// 	{
+// 		rt.width = 10;
+// 		rt.left = j;
+// 		aBitmap.Fill( 0x000000FF, &rt );	//Set different color
+// 		sprintf( tempFileName, "FillRect(left-%04d, width-10).bmp", j );
+// 		aBitmap.Save( tempFileName );
+// 	}
+
+// 	CHE_Bitmap bBitmap;
+// 	bBitmap.Load( "c:\\24.bmp" );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Blue, 0 );
+// 	bBitmap.Save( "c:\\24_NoBlue.bmp" );
+// 
+// 	bBitmap.Load( "c:\\24.bmp" );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Green, 0 );
+// 	bBitmap.Save( "c:\\24_NoGreen.bmp" );
+// 
+// 	bBitmap.Load( "c:\\24.bmp" );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Red, 0 );
+// 	bBitmap.Save( "c:\\24_NoRed.bmp" );
+// 
+// 	bBitmap.Load( "c:\\24.bmp" );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Blue, 0 );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Green, 0 );
+// 	bBitmap.Save( "c:\\24_OnlyRed.bmp" );
+// 
+// 	bBitmap.Load( "c:\\24.bmp" );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Blue, 0 );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Red, 0 );
+// 	bBitmap.Save( "c:\\24_OnlyGreen.bmp" );
+// 	
+// 	bBitmap.Load( "c:\\24.bmp" );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Green, 0 );
+// 	bBitmap.SetChannel( BITMAP_CHANNEL_Red, 0 );
+// 	bBitmap.Save( "c:\\24_OnlyBlue.bmp" );
 
 	return 0;
 }

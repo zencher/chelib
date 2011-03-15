2089e80d8e208b47a9db85fe1bed4d2f04d085ee
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-03-15 15:26:34 +0000

    类的修改
    git-svn-id: https://10.211.55.3/svn/chelib@63 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/src/pdf/che_pdf_xref.cpp
+++ trunk/src/pdf/che_pdf_xref.cpp
@@ -78,29 +78,29 @@ void CHE_PDF_XREF_Table::Clear()
 // 
 // }
 
-bool CHE_PDF_XREF_Table::Append( unsigned int offset, unsigned int objNum, unsigned int genNum, unsigned char flag )
-{
-	if ( m_pFirstEntry == NULL )
-	{
-		m_pFirstEntry = new PDF_XREF_ENTRY_NODE;
-		m_pFirstEntry->pNext = NULL;
-		m_pLastEntry = m_pFirstEntry;
-		m_pFirstEntry->entry.SetOffset( offset );
-		m_pFirstEntry->entry.SetObjNum( objNum );
-		m_pFirstEntry->entry.SetGenNum( genNum );
-		m_pFirstEntry->entry.SetFlag( flag );
-	}else{
-		m_pLastEntry->pNext = new PDF_XREF_ENTRY_NODE;
-		m_pLastEntry->pNext->pNext = NULL;
-		m_pLastEntry = m_pLastEntry->pNext;
-		m_pLastEntry->entry.SetOffset( offset );
-		m_pLastEntry->entry.SetObjNum( objNum );
-		m_pLastEntry->entry.SetGenNum( genNum );
-		m_pLastEntry->entry.SetFlag( flag );
-	}
-	m_lCount++;
-	return true;
-}
+// bool CHE_PDF_XREF_Table::Append( unsigned int offset, unsigned int objNum, unsigned int genNum, unsigned char flag )
+// {
+// 	if ( m_pFirstEntry == NULL )
+// 	{
+// 		m_pFirstEntry = new PDF_XREF_ENTRY_NODE;
+// 		m_pFirstEntry->pNext = NULL;
+// 		m_pLastEntry = m_pFirstEntry;
+// 		m_pFirstEntry->entry.SetOffset( offset );
+// 		m_pFirstEntry->entry.SetObjNum( objNum );
+// 		m_pFirstEntry->entry.SetGenNum( genNum );
+// 		m_pFirstEntry->entry.SetFlag( flag );
+// 	}else{
+// 		m_pLastEntry->pNext = new PDF_XREF_ENTRY_NODE;
+// 		m_pLastEntry->pNext->pNext = NULL;
+// 		m_pLastEntry = m_pLastEntry->pNext;
+// 		m_pLastEntry->entry.SetOffset( offset );
+// 		m_pLastEntry->entry.SetObjNum( objNum );
+// 		m_pLastEntry->entry.SetGenNum( genNum );
+// 		m_pLastEntry->entry.SetFlag( flag );
+// 	}
+// 	m_lCount++;
+// 	return true;
+// }
 
 bool CHE_PDF_XREF_Table::Append( CHE_PDF_XREF_Entry & entry )
 {
@@ -119,11 +119,11 @@ bool CHE_PDF_XREF_Table::Append( CHE_PDF_XREF_Entry & entry )
 	m_lCount++;
 	return true;
 }
-
-bool CHE_PDF_XREF_Table::Update( unsigned int offset, unsigned int objNum, unsigned int genNum, unsigned char flag )
-{
-	return false;
-}
+// 
+// bool CHE_PDF_XREF_Table::Update( unsigned int offset, unsigned int objNum, unsigned int genNum, unsigned char flag )
+// {
+// 	return false;
+// }
 
 bool CHE_PDF_XREF_Table::Update( CHE_PDF_XREF_Entry & entry )
 {

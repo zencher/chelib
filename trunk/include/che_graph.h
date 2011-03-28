26881795c5b69dfe3f14d4b2d76619055944ff4a
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-03-28 12:42:29 +0000

    git-svn-id: https://10.211.55.3/svn/chelib@85 5bab769a-80ee-f445-9178-c608493bda25

---
+++ trunk/include/che_graph.h
@@ -0,0 +1,13 @@
+#ifndef _CHE_GRAPH_H_
+#define _CHE_GRAPH_H_
+
+
+class CHE_Graphics;
+class CHE_Pen;
+class CHE_Font;
+class CHE_Color;
+class CHE_Point;
+class CHE_Rect;
+class CHE_GraphicsPath;
+
+#endif

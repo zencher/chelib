1acd81275a3fc41f3dd251ab1cae046aa12bb1a5
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-04-16 12:53:13 +0000

    添加了WCHAR的动态缓存类
    git-svn-id: https://10.211.55.3/svn/chelib@114 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/che_dynBuffer.h
+++ trunk/include/che_dynBuffer.h
@@ -28,12 +28,6 @@ public:
 
 	HE_VOID	Clear() { m_lByteCount = 0; }
 
-	//HE_DWORD Read( HE_DWORD offset, HE_LPBYTE  )
-
-	//HE_DWORD SetData( HE_DWORD offset, const HE_LPBYTE pBuffer, HE_DWORD size );
-
-	//HE_DWORD GetData( HE_DWORD offset, HE_LPBYTE pBuffer, HE_DWORD size );
-
 private:
 
 	HE_DWORD	m_lSize;
@@ -41,7 +35,41 @@ private:
 
 	HE_LPBYTE	m_lpData;
 	HE_DWORD	m_lByteCount;
-}; 
+};
+
+class CHE_DynWideByteBuffer : public CHE_Object
+{
+public:
+	CHE_DynWideByteBuffer( HE_DWORD size = 1024, HE_DWORD increament = 1024 );
+	CHE_DynWideByteBuffer( const CHE_DynWideByteBuffer & buf );
+	~CHE_DynWideByteBuffer();
+	
+	const CHE_DynWideByteBuffer & operator = ( const CHE_DynWideByteBuffer & buf );
+	
+	HE_LPWSTR GetData() { return m_lpData; }
+	
+	HE_DWORD GetSize() { return m_lSize; }
+	
+	HE_DWORD GetByteCount() { return m_lByteCount; }
+	
+	HE_DWORD Write( HE_LPCWSTR pBuffer, HE_DWORD size );
+	
+	HE_DWORD Read( HE_LPWSTR pBuffer, HE_DWORD size );
+	
+	HE_BOOL ReadByte( HE_DWORD offset, HE_LPWSTR pWcharRet );
+	
+	HE_DWORD Write( const CHE_DynWideByteBuffer & dynBuffer );
+	
+	HE_VOID	Clear() { m_lByteCount = 0; }
+	
+private:
+	
+	HE_DWORD	m_lSize;
+	HE_DWORD	m_lIncreament;
+	
+	HE_LPWSTR	m_lpData;
+	HE_DWORD	m_lByteCount;
+};
 
 
 #endif

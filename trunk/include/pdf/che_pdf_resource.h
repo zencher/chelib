7cdb75a19f11947e061b59a7a1695e79d2fb7636
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-05-15 13:29:32 +0000

    将CHE_PDF_FontCodeMgr类更改为包含字体所有相关内容封装的CHE_PDF_Font类
    git-svn-id: https://10.211.55.3/svn/chelib@140 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/pdf/che_pdf_resource.h
+++ trunk/include/pdf/che_pdf_resource.h
@@ -1,48 +1,5 @@
 #ifndef _CHE_PDF_RESOURCE_H_
 #define _CHE_PDF_RESOURCE_H_
 
-#include "../che_base.h"
-#include "che_pdf_objects.h"
-#include "che_pdf_parser.h"
-
-#define PDFFONT_UNKNOWN			0
-#define PDFFONT_TYPE0			1
-#define PDFFONT_TYPE1			2
-#define PDFFONT_TRUETYPE		3
-#define PDFFONT_TYPE3			4
-
-#define PDFENCODING_NONE		0
-#define PDFENCODING_STANDARD	1
-#define PDFENCODING_WINANSI		2
-#define	PDFENCODING_MACROMAN	3
-#define PDFENCODING_MACEXPERT	4
-#define PDFENCODING_PDFDOC		5
-#define PDFENCODING_GBK			6
-#define PDFENCODING_GBK_UCS2	7
-#define PDFENCODING_SELFDEF		8
-#define PDFENCODING_OTHER		9
-
-class CHE_PDF_FontCharCodeMgr : public CHE_Object
-{
-public:
-	CHE_PDF_FontCharCodeMgr( IHE_PDF_GetInObj * pIHE_GetPDFInObj, CHE_PDF_Dictionary * pFontDict );
-	~CHE_PDF_FontCharCodeMgr();
-
-	HE_WCHAR	GetUnicode( HE_WCHAR wch );
-
-	HE_BYTE		GetEncodingType() { return m_EncodingType; }
-	HE_BYTE		GetFontType() { return m_FontType; }
-	HE_BOOL		IsDefaultEncoding() { return m_bDefaultEncoding; }
-
-private:
-	IHE_PDF_GetInObj *		m_pIHE_GetPDFInObj;
-	CHE_PDF_Dictionary *	m_pFontDict;
-	HE_WCHAR *				m_pUnicodeTable;
-	CHE_NumToPtrMap*		m_pMap;
-
-	HE_BYTE					m_EncodingType;
-	HE_BYTE					m_FontType;
-	HE_BOOL					m_bDefaultEncoding;
-};
 
 #endif

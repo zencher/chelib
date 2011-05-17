f2bc043a023f2cf1590aa7a16bc8df623d3b52f1
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-05-17 10:38:32 +0000

    git-svn-id: https://10.211.55.3/svn/chelib@155 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/test/pdfdocument/main.cpp
+++ trunk/test/pdfdocument/main.cpp
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <cstdio>
 #include "../../include/pdf/che_pdf_pages.h"
-#include "../../include/che_dynBuffer.h"
+#include "../../include/che_dynbuffer.h"
 #include "../../include/pdf/che_pdf_renderer.h"
+#include "../../include/pdf/che_pdf_text.h"
 
 int main( int argc, char **argv )
 {
@@ -21,48 +22,79 @@ int main( int argc, char **argv )
 		printf( "%s \n",argv[1] );
 		printf( "file size : %d\n", pFileRead->GetSize() );
 	}
-	
-	CHE_PDF_Document doc;
-	doc.Load( pFileRead );
-	HE_DWORD lPageCount = doc.GetPageCount();
-	printf( "page count : %d\n", lPageCount );
-	CHE_PDF_Page * pTmpPage = NULL;
-	CHE_PDF_Renderer renderer;
-	//char tmpStr[1024];
-	//sprintf( tmpStr, "%s.txt", argv[1] );
-	//IHE_Write * pWrite = HE_CreateFileWrite( tmpStr );
-	HE_BOOL	bEncrypt = doc.IsEncrypted();
-	if ( bEncrypt )
-	{
-		//if ( pWrite )
-		//{
-		//	pWrite->WriteBlock( "Encrypted", 9 );
-		//	pWrite->Release();
-		//}
-		return 0;
-	}
-	for ( HE_DWORD i = 0; i < lPageCount; i++ )
-	{
-		if ( i == 228 )
+
+	try{
+		CHE_PDF_Document doc;
+		doc.Load( pFileRead );
+		HE_DWORD lPageCount = doc.GetPageCount();
+		printf( "page count : %d\n", lPageCount );
+		CHE_PDF_Page * pTmpPage = NULL;
+		char tmpStr[1024];
+		sprintf( tmpStr, "%s.text.txt", argv[1] );
+		IHE_Write * pWriteText = HE_CreateFileWrite( tmpStr );
+		HE_BYTE pTmp[2] = { 0xFF, 0xFE }; //UTF-16LE BOM
+		if ( pWriteText )
 		{
-			int x = 0;
+			pWriteText->WriteBlock( pTmp, 2 );
 		}
-		pTmpPage = doc.GetPage( i );
-		CHE_DynBuffer buffer;
-		renderer.GetPageContent( *pTmpPage, buffer );
-		HE_BYTE * pData = new HE_BYTE[buffer.GetByteCount()];
-		buffer.Read( pData,  buffer.GetByteCount() );
-		//if ( pWrite )
-		//{
-		//	pWrite->WriteBlock( pData, buffer.GetByteCount() );
-		//}
-// 		if ( pTmpPage )
-// 		{
-// 			printf( "page index : %d ", i+1 );
-// 			printf( "page width : %.2f ", pTmpPage->GetPageWidth() );
-// 			printf( "page height : %.2f\n", pTmpPage->GetPageHeight() );
-// 		}
+		sprintf( tmpStr, "%s.content.txt", argv[1] );
+		IHE_Write * pWriteContent = HE_CreateFileWrite( tmpStr );
+		for ( HE_DWORD i = 0; i < doc.GetPageCount(); i++ )
+		{
+			pTmpPage = doc.GetPage( i );
+			if ( pTmpPage == NULL )
+			{
+				printf( "page index : %d Error, Can not get the page!\n", i+1 );
+				continue;
+			}
+
+			CHE_DynBuffer buffer;
+			pTmpPage->GetPageContent( buffer );
+			if ( pWriteContent )
+			{
+				pWriteContent->WriteBlock( buffer.GetData(), buffer.GetByteCount() );
+			}
+
+			CHE_PDF_TextExtractor textExtractor;
+			CHE_DynWideByteBuffer buf(4096, 4096);
+			HE_DWORD lcount = textExtractor.Extract( pTmpPage, buf );
+			if ( pWriteText )
+			{
+				if ( sizeof(HE_WCHAR) == 2 )
+				{
+					pWriteText->WriteBlock( (HE_BYTE*)buf.GetData(), buf.GetByteCount() * 2 );
+				}else{
+					HE_CHAR ch;
+					for( HE_DWORD i = 0; i < buf.GetByteCount(); i++ )
+					{
+						ch = buf.GetData()[i] & 0xFF;
+						pWriteText->WriteByte( ch );
+						ch = (buf.GetData()[i] & 0xFF00) >> 8;
+						pWriteText->WriteByte( ch );	
+					}
+				}
+			}
+			if ( pTmpPage )
+			{
+				printf( "page index : %d ", i+1 );
+				printf( "page width : %.2f ", pTmpPage->GetPageWidth() );
+				printf( "page height : %.2f\n", pTmpPage->GetPageHeight() );
+				delete pTmpPage;
+			}
+		}
+		doc.Unload();
+		pWriteContent->Release();
+		pWriteText->Release();
+		pFileRead->Release();
+		HE_DestoryIHEWrite( pWriteContent );
+		HE_DestoryIHEWrite( pWriteText );
+		HE_DestoryIHERead( pFileRead );
+		pWriteContent = NULL;
+		pWriteText = NULL;
+		pFileRead = NULL;
+	}catch(...)
+	{
+		printf( "Error! Crash!\n" );
 	}
-//	pWrite->Release();
 	return 0;
 }

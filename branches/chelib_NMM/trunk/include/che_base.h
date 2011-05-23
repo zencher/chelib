4f7c00b0c64f77f1a38bf39e62a457d8773d3a36
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-05-23 08:03:27 +0000

    git-svn-id: https://10.211.55.3/svn/chelib@173 5bab769a-80ee-f445-9178-c608493bda25

--- branches/chelib_NMM/trunk/include/che_base.h
+++ branches/chelib_NMM/trunk/include/che_base.h
@@ -3,17 +3,7 @@
 
 #include "che_define.h"
 
-class CHE_Object
-{
-// public:
-// 	HE_LPVOID operator new( size_t );
-// 
-// 	HE_LPVOID operator new( size_t, HE_LPCSTR, HE_INT32 );
-// 
-// 	HE_VOID  operator delete( HE_LPVOID );
-// 
-// 	HE_VOID  operator delete( HE_LPVOID, HE_LPCSTR, HE_INT32 );
-};
+class CHE_Object {};
 
 class IHE_Pause
 {

03e985233f6c31034978d6238e617de6c7d8d8cc
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-05-23 12:00:10 +0000

    git-svn-id: https://10.211.55.3/svn/chelib@183 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/test/SimpleTest/chetest.cpp
+++ trunk/test/SimpleTest/chetest.cpp
@@ -1,12 +1,151 @@
-#include "../../include/che_dynBuffer.h"
+#include <cstdio>
+#include <memory.h>
+
+#include "../../include/che_bitmap.h"
+#include "../../include/che_datastructure.h"
 #include "../../include/pdf/che_pdf_parser.h"
 #include "../../include/pdf/che_pdf_filter.h"
-#include "../../include/che_bitmap.h"
-#include <stdio.h>
-#include <memory.h>
+#include "../../include/pdf/che_pdf_pages.h"
+#include "../../include/pdf/che_pdf_encrypt.h"
+
 
 int main()
 {
+	CHE_HeapAllocator gDefAlloc;
+	
+	//CHE_ByteString * pStr = NewArray<CHE_ByteString>( 2, gDefAlloc );
+
+	CHE_ByteString * pStr = gDefAlloc.New<CHE_ByteString>( "adsfadsf" );
+
+	gDefAlloc.Delete( pStr );
+
+	//*pStr = "sdfasd";
+
+	//delete pStr;
+	//DeleteArray( pStr, gDefAlloc );
+
+	//IHE_Read * pIHE_Read = HE_CreateFileRead( "D:\\PDFFiles\\AcroJS_7.0.5.pdf" ); //"C:\\EncryptTest.pdf"
+	//CHE_PDF_Parser parser;
+	//parser.StartParse( pIHE_Read );
+	//parser.ParseXRef();
+	//parser.Authenticate( CHE_ByteString("") );
+
+	//CHE_ByteString id1, id2;
+	//CHE_PDF_Dictionary * pRootDict = parser.GetTrailerDict();
+	//CHE_PDF_Array * pIDArray = (CHE_PDF_Array*)pRootDict->GetElement( CHE_ByteString("ID") );
+	//CHE_PDF_String * pID1 = (CHE_PDF_String*)pIDArray->GetElement( 0 );
+	//id1 = pID1->GetString();
+
+	//CHE_PDF_Object * pEncryptRef = pRootDict->GetElement( CHE_ByteString("Encrypt") );
+	//CHE_PDF_IndirectObject * pEncryptInObj = parser.GetIndirectObject( ((CHE_PDF_Reference*)pEncryptRef)->GetRefNuml() );
+//	CHE_PDF_Dictionary * pEncryptDict = pEncryptInObj->GetDict();
+	
+	//HE_BYTE OValue[32];
+	//HE_BYTE UValue[32];
+
+	//CHE_PDF_Object * pElement = pEncryptDict->GetElement( CHE_ByteString("O") );
+	//CHE_ByteString stro = ((CHE_PDF_String*)pElement)->GetString();
+	//pElement = pEncryptDict->GetElement( CHE_ByteString("U") );
+	//CHE_ByteString stru = ((CHE_PDF_String*)pElement)->GetString();
+	//for( HE_DWORD i = 0 ; i < stro.GetLength(); i++ )
+	//{
+	//	OValue[i] = stro[i];
+//	}
+	//for( HE_DWORD j = 0 ; j < stru.GetLength(); j++ )
+	//{
+	//	UValue[j] = stru[j];
+	//}
+
+	//CHE_PDF_Encrypt encrypt1( id1, OValue, UValue, 2, 128, 3, TRUE, 0 );
+	//HE_BOOL bRightPassword = encrypt1.Authenticate( CHE_ByteString("456") );
+
+// 	CHE_PDF_IndirectObject * pInObj = parser.GetIndirectObject( 102293 );
+//  	CHE_PDF_Stream * pStream = pInObj->GetStream();
+//  	CHE_PDF_StreamAcc stmAcc;
+//  	stmAcc.Attach( pStream );
+//  	stmAcc.GetData();
+// 	IHE_Write * pWrite = HE_CreateFileWrite( "c:\\102293.txt" );
+// 	pWrite->WriteBlock( (HE_LPVOID)stmAcc.GetData(), stmAcc.GetSize() );
+// 	pWrite->Flush();
+// 	HE_DestoryIHEWrite( pWrite );
+// 	pWrite = NULL;
+
+// 	CHE_PDF_Dictionary*  pDict = (CHE_PDF_Dictionary*)pInObj->GetObject();
+// 	CHE_PDF_String * pObj = (CHE_PDF_String*)pDict->GetElement( CHE_ByteString("Producer") );
+// 	CHE_ByteString str = pObj->GetString();
+
+	//encrypt1.Decrypt( str, 13, 0 );
+
+//	pInObj = parser.GetIndirectObject( 10 );
+//	CHE_PDF_Stream * pStm = pInObj->GetStream();
+//	HE_BYTE * pRawData = new HE_BYTE[pStm->GetRawSize()];
+//	pStm->ReadRawData( 0, pRawData, pStm->GetRawSize() );
+//
+//	HE_DWORD lengthRet = encrypt1.Decrypt( pRawData, pStm->GetRawSize(), 10, 0 );
+//
+//	CHE_PDF_FlateFilter flateFliter;
+//	CHE_DynBuffer buf;
+//	flateFliter.Decode( pRawData, lengthRet, buf );
+
+	
+		/*IHE_Read * pIHE_Read = HE_CreateFileRead( "D:\\1.pdf" );
+	CHE_PDF_Document doc;
+	doc.Load( pIHE_Read );
+
+	CHE_ByteString id1, id2;
+	doc.GetID( id1, id2 );
+	
+	CHE_PDF_Object * pObj = doc.GetParser()->GetTrailerDict()->GetElement( CHE_ByteString("Encrypt") );
+	if ( pObj == NULL || pObj->GetType() != PDFOBJ_REFERENCE )
+	{
+		return 0;
+	}
+	CHE_PDF_IndirectObject * pInObj = doc.GetParser()->GetIndirectObject( ((CHE_PDF_Reference*)pObj)->GetRefNuml() );
+	pObj = pInObj->GetDict();
+	if ( pObj->GetType() != PDFOBJ_DICTIONARY )
+	{
+		return 0;
+	}
+	pObj = ((CHE_PDF_Dictionary*)pObj)->GetElement( CHE_ByteString("O") );
+	CHE_ByteString o = ((CHE_PDF_String*)pObj)->GetString();
+
+	
+	CHE_PDF_Encrypt encrypt;
+	bool ok;
+
+	for ( int i = 0; i < o.GetLength(); i++ )
+	{
+		encrypt.m_oValue[i] = o[i];
+	}
+
+
+	encrypt.m_documentId = id1;
+	//encrypt.m_oValue = "Mær™æU?ëùq÷µÁðqÑûR¤?.";
+	encrypt.m_pValue = -1340;
+	encrypt.m_keyLength = 128 / 8;
+	encrypt.m_rValue = 4;
+    // Pad password
+    unsigned char userKey[32];
+    unsigned char pswd[32];
+    encrypt.PadPassword( CHE_ByteString(""), pswd );
+	
+    // Check password: 1) as user password, 2) as owner password
+    encrypt.ComputeEncryptionKey( encrypt.m_documentId, pswd, encrypt.m_oValue, encrypt.m_pValue, encrypt.m_keyLength * 8, encrypt.m_rValue, userKey);
+	
+    ok = encrypt.CheckKey(userKey, encrypt.m_uValue);
+    if (!ok)
+    {
+        unsigned char userpswd[32];
+        encrypt.ComputeOwnerKey( encrypt.m_oValue, pswd, encrypt.m_keyLength, encrypt.m_rValue, true, userpswd );
+        encrypt.ComputeEncryptionKey( encrypt.m_documentId, userpswd, encrypt.m_oValue, encrypt.m_pValue, encrypt.m_keyLength * 8, encrypt.m_rValue, userKey );
+        ok = encrypt.CheckKey( userKey, encrypt.m_oValue );
+		
+        if( ok ) 
+            encrypt.m_ownerPass = "";
+    }
+    else
+        encrypt.m_userPass = "";
+*/
 	//CHE_DynBuffer dynBuffer;
 
 	//CHE_PDF_RLEFileter filter;
@@ -118,21 +257,21 @@ int main()
 
 
 
-	IHE_FileRead * pFileRead = HE_CreateFileRead( "c:\\pdf.pdf" );
-	if ( pFileRead )
-	{
-		CHE_PDF_Parser parser;
-		parser.StartParse( pFileRead );
-		HE_DWORD version = parser.GetPDFVersion();
-		HE_DWORD xrefoffset = parser.GetStartxrefOffset( 512 );
-		HE_BOOL bRet = parser.GetXRefTable();
-		//CHE_PDF_IndirectObject * pObj = parser.GetIndirectObject( 1 );
-		//CHE_PDF_Dictionary* pDict = (CHE_PDF_Dictionary*)(pObj->GetDict()->GetElement( CHE_ByteString("OCProperties") ));
-		//CHE_PDF_Array* pArr = (CHE_PDF_Array*)(pDict->GetElement( CHE_ByteString("OCGs") ) );
-		//CHE_PDF_Dictionary* pDict2 = (CHE_PDF_Dictionary*)(pDict->GetElement( CHE_ByteString("D") ) );
-		//pDict->Release();
-		//int x = 0;
-		HE_DWORD pageCount = parser.GetPageCount();
+// 	IHE_FileRead * pFileRead = HE_CreateFileRead( "c:\\pdf.pdf" );
+// 	if ( pFileRead )
+// 	{
+// 		CHE_PDF_Parser parser;
+// 		parser.StartParse( pFileRead );
+// 		HE_DWORD version = parser.GetPDFVersion();
+// 		HE_DWORD xrefoffset = parser.GetStartxrefOffset( 512 );
+// 		HE_BOOL bRet = parser.GetXRefTable();
+// 		//CHE_PDF_IndirectObject * pObj = parser.GetIndirectObject( 1 );
+// 		//CHE_PDF_Dictionary* pDict = (CHE_PDF_Dictionary*)(pObj->GetDict()->GetElement( CHE_ByteString("OCProperties") ));
+// 		//CHE_PDF_Array* pArr = (CHE_PDF_Array*)(pDict->GetElement( CHE_ByteString("OCGs") ) );
+// 		//CHE_PDF_Dictionary* pDict2 = (CHE_PDF_Dictionary*)(pDict->GetElement( CHE_ByteString("D") ) );
+// 		//pDict->Release();
+// 		//int x = 0;
+// 		HE_DWORD pageCount = parser.GetPageCount();
 		
 // 		parser.CloseParser();
 // 
@@ -148,7 +287,6 @@ int main()
 // 		//	syntaxParser.NextLine();
 // 			int x = 0;
 // 		}
-
-	}
-	return 0;
+//	}
+ 	return 0;
 }

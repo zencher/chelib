3e4210a3f34add7b2af710e08b6d9329db82b9b2
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2011-05-30 08:12:44 +0000

    设计并实现了基础的图形类（直线，贝塞尔曲线，矩形，路径）
    git-svn-id: https://10.211.55.3/svn/chelib@193 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/che_graphics.h
+++ trunk/include/che_graphics.h
@@ -11,54 +11,80 @@
 //class CHE_Point;
 //class CHE_Rect;
 
-#define GRAPHTYPE_LINE			0
-#define GRAPHTYPE_RECTANGLE		1
-#define GRAPHTYPE_PATH			2
+#define GRAPHTYPE_INVALID		0
+#define GRAPHTYPE_LINE			1
+#define GRAPHTYPE_BCURVE		2
+#define GRAPHTYPE_RECTANGLE		3
+#define GRAPHTYPE_PATH			4
 
 class CHE_GraphicsObject : public CHE_Object
 {
 public :
-	CHE_GraphicsObject( CHE_Allocator * pAllocator = NULL ) : CHE_Object( pAllocator ) {}
-	HE_BYTE	GetType() { return m_type; } 
+	CHE_GraphicsObject( HE_BYTE type = GRAPHTYPE_INVALID, CHE_Allocator * pAllocator = NULL ) : CHE_Object( pAllocator ) { m_type = type; }
+
+	HE_BYTE	GetType() { return m_type; }
+
+	HE_VOID Release();
+
+private:
 	HE_BYTE m_type;
 };
 
 class CHE_Line : public CHE_GraphicsObject
 {
 public:
-	CHE_Line( HE_LONG lStartX, HE_LONG lStartY, HE_LONG lEndX, HE_LONG lEndY, CHE_Allocator * pAllocator = NULL ) : CHE_GraphicsObject( pAllocator )
-		{ m_lStartX = lStartX; m_lStartY = lStartY; m_lEndX = lEndX; m_lEndY = lEndY; m_type = GRAPHTYPE_LINE; }
-	HE_LONG m_lStartX;
-	HE_LONG m_lStartY;
-	HE_LONG m_lEndX;
-	HE_LONG m_lEndY;
+	CHE_Line( HE_FLOAT StartX, HE_FLOAT StartY, HE_FLOAT EndX, HE_FLOAT EndY, CHE_Allocator * pAllocator = NULL ) 
+		: CHE_GraphicsObject( GRAPHTYPE_LINE, pAllocator )
+	{ m_fStartX = StartX; m_fStartY = StartY; m_fEndX = EndX; m_fEndY = EndY; }
+	HE_FLOAT m_fStartX;
+	HE_FLOAT m_fStartY;
+	HE_FLOAT m_fEndX;
+	HE_FLOAT m_fEndY;
+};
+
+class CHE_BCurve : public CHE_GraphicsObject
+{
+public:
+	CHE_BCurve( HE_FLOAT X1, HE_FLOAT Y1, HE_FLOAT X2, HE_FLOAT Y2, HE_FLOAT X3, HE_FLOAT Y3,
+				HE_FLOAT X4, HE_FLOAT Y4, CHE_Allocator * pAllocator = NULL )
+		: CHE_GraphicsObject( GRAPHTYPE_BCURVE, pAllocator )
+	{ m_fX1 = X1, m_fY1 = Y1; m_fX2 = X2; m_fY2 = Y2; m_fX3 = X3; m_fY3 = Y3; m_fX4 = X4; m_fY4 = Y4; }
+
+	HE_FLOAT m_fX1;
+	HE_FLOAT m_fY1;
+	HE_FLOAT m_fX2;
+	HE_FLOAT m_fY2;
+	HE_FLOAT m_fX3;
+	HE_FLOAT m_fY3;
+	HE_FLOAT m_fX4;
+	HE_FLOAT m_fY4;
 };
 
 class CHE_Rectangle : public CHE_GraphicsObject
 {
 public:
-	CHE_Rectangle( HE_LONG leftTopX, HE_LONG leftTopY, HE_DWORD dwWidth, HE_DWORD dwHeight, CHE_Allocator * pAllocator = NULL ) : CHE_GraphicsObject( pAllocator )
-		{ m_lLeftTopX = leftTopX; m_lLeftTopY = leftTopY; m_dwWidth = dwWidth; m_dwHeight = dwHeight;m_type = GRAPHTYPE_RECTANGLE;  }
+	CHE_Rectangle( HE_FLOAT leftTopX, HE_FLOAT leftTopY, HE_FLOAT width, HE_FLOAT height, CHE_Allocator * pAllocator = NULL ) : CHE_GraphicsObject( GRAPHTYPE_RECTANGLE, pAllocator )
+		{ m_fLeftTopX = leftTopX; m_fLeftTopY = leftTopY; m_fWidth = width; m_fHeight = height; }
 	
-	HE_LONG m_lLeftTopX;
-	HE_LONG m_lLeftTopY;
-	HE_DWORD m_dwWidth;
-	HE_DWORD m_dwHeight;
+	HE_FLOAT m_fLeftTopX;
+	HE_FLOAT m_fLeftTopY;
+	HE_FLOAT m_fWidth;
+	HE_FLOAT m_fHeight;
 };
-
-// class CHE_BezierLine : public CHE_GraphicsObject
-// {
-// 
-// };
 
 class CHE_Path : public CHE_GraphicsObject
 {
 public:
-	CHE_Path( CHE_Allocator * pAllocator = NULL ) : CHE_GraphicsObject( pAllocator ), m_objArray( pAllocator ) {}
+	CHE_Path( CHE_Allocator * pAllocator = NULL ) : CHE_GraphicsObject( GRAPHTYPE_PATH, pAllocator ), m_objArray( pAllocator ) {}
 
 	HE_VOID		AddLine( CHE_Line & line );
+	
+	HE_VOID		AddBCrave( CHE_BCurve & crave );
+	
 	HE_VOID		AddRectangle( CHE_Rectangle & rect );
+
 	CHE_GraphicsObject * GetGraph( HE_DWORD index ) { return (CHE_GraphicsObject *)m_objArray.GetItem( index ); }
+	
 	HE_DWORD	GetGraphCount() { return m_objArray.GetCount(); }
 
 private:

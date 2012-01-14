6c52f7be590157ad05b3a698425114fad7e481d6
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2012-01-14 08:40:18 +0000

    PDF Matrix的基础定义
    git-svn-id: https://10.211.55.3/svn/chelib@315 5bab769a-80ee-f445-9178-c608493bda25

---
+++ trunk/include/pdf/che_pdf_matrix.h
@@ -0,0 +1,118 @@
+#ifndef _CHE_PDF_MATRIX_H_
+#define _CHE_PDF_MATRIX_H_
+#include <cmath>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
+class CHE_PDF_Matrix
+{
+public:
+	CHE_PDF_Matrix() : a(1), b(0), c(0), d(1), e(0), f(0) {}
+	CHE_PDF_Matrix( float va, float vb, float vc, float vd, float ve, float vf )
+		: a(va), b(vb), c(vc), d(vd), e(ve), f(vf) {}
+
+	static CHE_PDF_Matrix TranslateMatrix( HE_FLOAT tx, HE_FLOAT ty )
+	{
+		CHE_PDF_Matrix matrixRet;
+		matrixRet.e = tx;
+		matrixRet.f = ty;
+		return matrixRet;
+	}
+
+	static CHE_PDF_Matrix RotateMatrix( HE_FLOAT theta )
+	{
+		float s;
+		float c;
+		CHE_PDF_Matrix matrixRet;
+
+		while ( theta < 0 )
+			theta += 360;
+		while ( theta >= 360 )
+			theta -= 360;
+
+		if ( fabsf(0 - theta) < FLT_EPSILON )
+		{
+			s = 0;
+			c = 1;
+		}
+		else if ( fabsf(90.0f - theta) < FLT_EPSILON )
+		{
+			s = 1;
+			c = 0;
+		}
+		else if ( fabsf(180.0f - theta) < FLT_EPSILON )
+		{
+			s = 0;
+			c = -1;
+		}
+		else if ( fabsf(270.0f - theta) < FLT_EPSILON )
+		{
+			s = -1;
+			c = 0;
+		}
+		else
+		{
+			s = sinf(theta * (float)M_PI / 180);
+			c = cosf(theta * (float)M_PI / 180);
+		}
+
+		matrixRet.a = c; matrixRet.b = s;
+		matrixRet.c = -s; matrixRet.d = c;
+		matrixRet.e = 0; matrixRet.f = 0;
+		return matrixRet;
+	}
+
+	static CHE_PDF_Matrix ScaleMatrix( HE_FLOAT sx, HE_FLOAT sy )
+	{
+		CHE_PDF_Matrix matrixRet;
+		matrixRet.a = sx;
+		matrixRet.d = sy;
+		return matrixRet;
+	}
+
+	static CHE_PDF_Matrix SkewMatrix( HE_FLOAT a, HE_FLOAT b )
+	{
+		CHE_PDF_Matrix matrixRet;
+		matrixRet.b = tanf(a);
+		matrixRet.c = tanf(b);
+		return matrixRet;
+	}
+
+	void Concat( const CHE_PDF_Matrix & matrix )
+	{
+		a = a * matrix.a + b * matrix.c;
+		b = a * matrix.b + b * matrix.d;
+		c = c * matrix.a + d * matrix.c;
+		d = c * matrix.b + d * matrix.d;
+		e = e * matrix.a + f * matrix.c + matrix.e;
+		f = e * matrix.b + f * matrix.d + matrix.f;
+	}
+
+	void Invert( const CHE_PDF_Matrix & matirx )
+	{
+		float rdet = matirx.AbsValue();
+		a = matirx.d * rdet;
+		b = - matirx.b * rdet;
+		c = - matirx.c * rdet;
+		d = matirx.a * rdet;
+		e = - matirx.e * a - matirx.f * c;
+		f = - matirx.e * b - matirx.f * d;
+	}
+
+	HE_FLOAT AbsValue() const
+	{
+		HE_FLOAT val = 1 / ( a * d - b * c );
+		return val;
+	}
+
+	float a;
+	float b;
+	float c;
+	float d;
+	float e;
+	float f;
+};
+
+#endif

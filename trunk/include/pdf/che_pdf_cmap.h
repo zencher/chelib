160a95150a5e47f2ebae35dc18a52e1c47701966
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2012-02-27 00:07:29 +0000

    完善CMap的实现
    git-svn-id: https://10.211.55.3/svn/chelib@337 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/pdf/che_pdf_cmap.h
+++ trunk/include/pdf/che_pdf_cmap.h
@@ -2,15 +2,26 @@
 #define _CHE_PDF_CMAP_H_
 
 #include "../che_base.h"
+#include "../che_string.h"
+#include "che_pdf_cmap_res.h"
 
 class CHE_PDF_CMap : public CHE_Object
 {
 public:
-	CHE_PDF_CMap( CHE_Allocator * pAllocator = NULL ) : CHE_Object( pAllocator ) {}
+	static CHE_PDF_CMap * LoadBuildinCMap( const CHE_ByteString & cmapName,  CHE_Allocator * pAllocator = NULL );
 
-	~CHE_PDF_CMap() { }
+	~CHE_PDF_CMap();
+
+	HE_INT32 LookupCode( HE_INT32 cpt ) const;
 
 private:
+	CHE_PDF_CMap( PDF_CMAP * pCmap, HE_BOOL bNeedClear = FALSE, CHE_Allocator * pAllocator = NULL )
+		: CHE_Object(pAllocator), mbNeedClear(FALSE) { mpCMap = pCmap; }
+
+	HE_BOOL		mbNeedClear; 
+	PDF_CMAP *	mpCMap;
+
+	friend CHE_Allocator;
 };
 
 #endif

c8c0be467aec07ad5237f0d66c01973fd5c75306
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2012-02-29 14:07:50 +0000

    实现了字体管理器的雏形
    git-svn-id: https://10.211.55.3/svn/chelib@346 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/pdf/che_pdf_fontmgr.h
+++ trunk/include/pdf/che_pdf_fontmgr.h
@@ -3,12 +3,19 @@
 
 #include "../che_base.h"
 #include "che_pdf_define.h"
+#include "che_pdf_font.h"
 
 class CHE_PDF_FontMgr : public CHE_Object
 {
 public:
-	CHE_PDF_FontMgr(  CHE_Allocator * pAllocator = NULL );
+	CHE_PDF_FontMgr(  CHE_Allocator * pAllocator = NULL )
+		: CHE_Object( pAllocator) {};
 	~CHE_PDF_FontMgr();
+
+	CHE_PDF_Font * LoadFont( CHE_PDF_Reference * pReference );
+
+private:
+	CHE_NumToPtrMap mNumToFontMap;
 };
 
 #endif
--- trunk/src/pdf/che_pdf_fontmgr.cpp
+++ trunk/src/pdf/che_pdf_fontmgr.cpp
@@ -1,1 +1,49 @@
 #include "../../include/pdf/che_pdf_fontmgr.h"
+#include "../../include/che_datastructure.h"
+
+CHE_PDF_FontMgr::~CHE_PDF_FontMgr()
+{
+	HE_LPCVOID lpVoid = NULL;
+	CHE_PDF_Font * pFont = NULL;
+	for ( HE_DWORD i = 0; i < mNumToFontMap.GetCount(); ++i )
+	{
+		lpVoid = mNumToFontMap.GetItemByIndex( i );
+		if ( lpVoid )
+		{
+			pFont = (CHE_PDF_Font *)( lpVoid );
+			pFont->GetAllocator()->Delete( pFont ); 
+		}
+	}
+	mNumToFontMap.Clear();
+}
+
+CHE_PDF_Font * CHE_PDF_FontMgr::LoadFont( CHE_PDF_Reference * pReference )
+{
+	if ( pReference == NULL )
+	{
+		return NULL;
+	}
+	HE_DWORD objNum = pReference->GetRefNum();
+	HE_DWORD genNum = pReference->GetGenNum();
+	HE_DWORD tmpValue = ( objNum << 5 ) + genNum;
+	HE_LPCVOID lpVoid = mNumToFontMap.GetItem( tmpValue );
+	if ( lpVoid == NULL )
+	{
+		CHE_PDF_Object * pTmpObj = pReference->GetRefObj( OBJ_TYPE_DICTIONARY );
+		if ( pTmpObj == NULL )
+		{
+			return NULL;
+		}
+		CHE_PDF_Dictionary * pDict = pTmpObj->ToDict();
+		if ( pDict )
+		{
+			CHE_PDF_Font * pTmpFont = GetAllocator()->New<CHE_PDF_Font>( pDict, GetAllocator() );
+			if ( pTmpFont )
+			{
+				mNumToFontMap.Append( tmpValue, (HE_LPVOID)( pTmpFont ) );
+				return pTmpFont;
+			}
+		}
+	}
+	return (CHE_PDF_Font*)(lpVoid);
+}

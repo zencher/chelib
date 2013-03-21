03970b14fa4b278a7850e1cc8c6c4acb7cdb3511
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2013-03-21 14:40:28 +0000

    为矩阵类加入了一些运行接口
    git-svn-id: https://10.211.55.3/svn/chelib@571 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/pdf/che_pdf_matrix.h
+++ trunk/include/pdf/che_pdf_matrix.h
@@ -8,6 +8,86 @@
 #define M_PI 3.14159265358979323846
 #endif
 
+class CHE_PDF_Point
+{
+public:
+	CHE_PDF_Point() : x(0), y(0) {}
+	CHE_PDF_Point( HE_FLOAT vx, HE_FLOAT vy ) : x(vx), y(vy) {}
+
+	HE_FLOAT	x;
+	HE_FLOAT	y;
+};
+
+class CHE_PDF_Rect
+{
+public:
+	CHE_PDF_Rect() : left(0), bottom(0), width(0), height(0) {}
+	CHE_PDF_Rect( HE_FLOAT vleft, HE_FLOAT vbottom, HE_FLOAT vwidth, HE_FLOAT vheight )
+		: left(vleft), bottom(vbottom), width(vwidth), height(vheight) {}
+
+	HE_BOOL	IsEmpty() const
+	{
+		return ( fabs(width) < FLT_EPSILON || fabs(height) < FLT_EPSILON );
+	}
+
+	HE_VOID	Union( const CHE_PDF_Rect & rect )
+	{
+		if ( &rect != this )
+		{
+			if ( IsEmpty() )
+			{
+				*this = rect;
+				return;
+			}
+			if ( !rect.IsEmpty() )
+			{
+				CHE_PDF_Rect tmpRect;
+				tmpRect.left = ( left <= rect.left ) ? left : rect.left;
+				tmpRect.bottom = ( bottom <= rect.bottom ) ? bottom : rect.bottom;
+				tmpRect.width = ( left + width >= rect.left + rect.width ) ? left + width : rect.left + rect.width;
+				tmpRect.width -= tmpRect.left;
+				tmpRect.height = ( bottom + height >= rect.bottom + rect.height ) ? bottom + height : rect.bottom + rect.height;
+				tmpRect.height -= tmpRect.bottom;
+				*this = tmpRect;
+			}
+		}
+	}
+
+	bool operator == ( const CHE_PDF_Rect & rect ) const
+	{
+		if (	fabsf( left - rect.left ) <= FLT_EPSILON &&
+				fabsf( bottom - rect.bottom ) <= FLT_EPSILON &&
+				fabsf( width - rect.width ) <= FLT_EPSILON &&
+				fabsf( height - rect.height ) <= FLT_EPSILON )
+		{
+			return true;
+		}
+		return false;
+	}
+
+	bool operator != ( const CHE_PDF_Rect & rect ) const
+	{
+		return ! operator==( rect );
+	}
+
+	HE_FLOAT	left;
+	HE_FLOAT	bottom;
+	HE_FLOAT	width;
+	HE_FLOAT	height;
+};
+
+
+#define HE_MAX4(a,b,c,d) HE_max(HE_max(a,b), HE_max(c,d))
+#define HE_MIN4(a,b,c,d) HE_min(HE_min(a,b), HE_min(c,d))
+static inline float HE_min(float a, float b)
+{
+	return (a < b ? a : b);
+}
+static inline float HE_max(float a, float b)
+{
+	return (a > b ? a : b);
+}
+
 class CHE_PDF_Matrix
 {
 public:
@@ -110,6 +190,39 @@ public:
 		return val;
 	}
 
+	CHE_PDF_Point Transform( const CHE_PDF_Point & point ) const
+	{
+		CHE_PDF_Point pointRet;
+		pointRet.x = point.x * a + point.y * c + e;
+		pointRet.y = point.x * b + point.y * d + f;
+		return pointRet;
+	}
+
+	CHE_PDF_Rect Transform( const CHE_PDF_Rect & rect ) const
+	{
+		CHE_PDF_Point s, t, u, v;	
+		CHE_PDF_Rect retRect;
+
+		s.x = rect.left;
+		s.y = rect.bottom;
+		t.x = rect.left;
+		t.y = rect.bottom + rect.height;
+		u.x = rect.left + rect.width;
+		u.y = rect.bottom + rect.height;
+		v.x = rect.left + rect.width;
+		v.y = rect.bottom;
+		s = Transform( s );
+		t = Transform( t );
+		u = Transform( u );
+		v = Transform( v );
+		
+		retRect.left = HE_MIN4(s.x, t.x, u.x, v.x);
+		retRect.bottom = HE_MIN4(s.y, t.y, u.y, v.y);
+		retRect.width = HE_MAX4(s.x, t.x, u.x, v.x) - HE_MIN4(s.x, t.x, u.x, v.x);
+		retRect.height = HE_MAX4(s.y, t.y, u.y, v.y) - HE_MIN4(s.y, t.y, u.y, v.y);
+		return retRect;
+	}
+
 	bool operator == ( const CHE_PDF_Matrix & matrix ) const
 	{
 		if ( fabsf( a - matrix.a ) <= FLT_EPSILON &&
@@ -119,9 +232,9 @@ public:
 			 fabsf( e - matrix.e ) <= FLT_EPSILON &&
 			 fabsf( f - matrix.f ) <= FLT_EPSILON )
 		{
-			return TRUE;
+			return true;
 		}
-		return FALSE;
+		return false;
 	}
 
 	bool operator != ( const CHE_PDF_Matrix & matrix ) const

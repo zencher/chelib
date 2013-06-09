137cd62bd7ee0d6629bebb1268ac80a62a4364fe
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2013-06-09 06:10:39 +0000

    晚上了颜色空间的处理，iccbase的calgray，calrgb，calcmyk都用device的colorspace替代，lab的运算方法来自mupuf，现在还剩下index，deviveN，sepration这三个，这其中后两个需要用到pdf中的function，需要完善，后期需要晚上完成icc颜色空间的处理。这样，整个颜色空间的处理就都完成了。
    git-svn-id: https://10.211.55.3/svn/chelib@645 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/src/pdf/che_pdf_colorspace.cpp
+++ trunk/src/pdf/che_pdf_colorspace.cpp
@@ -1,4 +1,5 @@
 #include "../../include/pdf/che_pdf_colorspace.h"
+#include <math.h>
 
 HE_BOOL CHE_PDF_ColorSpace::IsDeviceColorSpace()
 {
@@ -15,6 +16,20 @@ HE_BOOL CHE_PDF_ColorSpace::IsDeviceColorSpace()
 	return FALSE;;
 }
 
+
+
+static inline float fung(float x)
+{
+    if (x >= 6.0f / 29.0f)
+        return x * x * x;
+    return (108.0f / 841.0f) * (x - (4.0f / 29.0f));
+}
+
+static inline float fz_clamp(float f, float min , float max)
+{
+    return (f > min ? ( f < max ? f : max) : min );
+}
+
 HE_UINT32 CHE_PDF_ColorSpace::GetArgb( CHE_PDF_Color & color )
 {
 	switch ( mType )
@@ -26,7 +41,7 @@ HE_UINT32 CHE_PDF_ColorSpace::GetArgb( CHE_PDF_Color & color )
 			HE_UINT32 tmpByte = 0x00;
 			if ( color.mConponents.size() >= 1 )
 			{
-				tmpByte = (HE_UINT32)( color.mConponents[0] * 255 );
+				tmpByte = color.mConponents[0] * 255;
 				valRet = valRet | tmpByte | tmpByte << 8 | tmpByte << 16;
 			}
 			return valRet;
@@ -40,9 +55,9 @@ HE_UINT32 CHE_PDF_ColorSpace::GetArgb( CHE_PDF_Color & color )
 			HE_UINT32 tmpByte3 = 0x00;
 			if ( color.mConponents.size() >= 3 )
 			{
-				tmpByte1 = (HE_UINT32)( color.mConponents[0] * 255 );
-				tmpByte2 = (HE_UINT32)( color.mConponents[1] * 255 );
-				tmpByte3 = (HE_UINT32)( color.mConponents[2] * 255 );
+				tmpByte1 = color.mConponents[0] * 255;
+				tmpByte2 = color.mConponents[1] * 255;
+				tmpByte3 = color.mConponents[2] * 255;
 				valRet = valRet | tmpByte1 << 16 | tmpByte2 << 8 | tmpByte3;
 			}
 			return valRet;
@@ -58,22 +73,161 @@ HE_UINT32 CHE_PDF_ColorSpace::GetArgb( CHE_PDF_Color & color )
 				bgr[2] = ( 1 < color.mConponents[0] + color.mConponents[3] ) ? color.mConponents[0] + color.mConponents[3] : 1;
 				
 				HE_UINT32 valRet = 0xFF000000;
-				HE_UINT32 tmpByte1 = 0x00;
-				HE_UINT32 tmpByte2 = 0x00;
-				HE_UINT32 tmpByte3 = 0x00;
-				if ( color.mConponents.size() >= 3 )
-				{
-					tmpByte1 = (HE_UINT32)( bgr[0] * 255 );
-					tmpByte2 = (HE_UINT32)( bgr[1] * 255 );
-					tmpByte3 = (HE_UINT32)( bgr[2] * 255 );
-					valRet = valRet | tmpByte1 << 16 | tmpByte2 << 8 | tmpByte3;
-				}
+				HE_UINT32 tmpByte1 = bgr[0] * 255;
+				HE_UINT32 tmpByte2 = bgr[1] * 255;
+				HE_UINT32 tmpByte3 = bgr[2] * 255;
+                valRet = valRet | tmpByte1 << 16 | tmpByte2 << 8 | tmpByte3;
 				return valRet;
 			}
 		}
+    case COLORSPACE_CIEBASE_CALLAB:
+        {
+            if ( color.mConponents.size() >= 3 )
+			{
+                HE_FLOAT lab[3];
+                HE_FLOAT range[6] = { -100, 100, -100, 100, -100, 100 };
+                for ( HE_BYTE i = 0 ; i < 3; ++i )
+                {
+                    lab[i] = color.mConponents[i];
+                }
+                
+                if ( mpObj && mpObj->GetType() == OBJ_TYPE_ARRAY )
+                {
+                    CHE_PDF_ArrayPtr arrayPtr = mpObj->GetArrayPtr();
+                    CHE_PDF_ObjectPtr objPtr = arrayPtr->GetElement( 2, OBJ_TYPE_DICTIONARY );
+                    if ( objPtr )
+                    {
+                        CHE_PDF_DictionaryPtr dictPtr = objPtr->GetDictPtr();
+                        objPtr = dictPtr->GetElement( "Range", OBJ_TYPE_ARRAY );
+                        if ( objPtr )
+                        {
+                            arrayPtr = objPtr->GetArrayPtr();
+                            if ( arrayPtr->GetCount() >= 4 )
+                            {
+                                for ( HE_ULONG i = 0 ; i < 4; ++i )
+                                {
+                                    objPtr = arrayPtr->GetElement( i+2, OBJ_TYPE_NUMBER );
+                                    if ( objPtr )
+                                    {
+                                        range[i+2] = objPtr->GetNumberPtr()->GetInteger();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                lab[0] = lab[0] * ( range[1] - range[0]) + fabs( range[0] );
+                lab[1] = lab[1] * ( range[3] - range[2]) + fabs( range[2] );
+                lab[2] = lab[2] * ( range[5] - range[4]) + fabs( range[4] );
+                
+                HE_FLOAT lstar , astar, bstar, l , m, n, x , y, z, r , g, b;
+                lstar = lab [0];
+                astar = lab [1];
+                bstar = lab [2];
+                m = (lstar + 16) / 116;
+                l = m + astar / 500;
+                n = m - bstar / 200;
+                x = fung (l);
+                y = fung (m);
+                z = fung (n);
+                r = (3.240449f * x + -1.537136f * y + -0.498531f * z) * 0.830026f;
+                g = (-0.969265f * x + 1.876011f * y + 0.041556f * z) * 1.05452f;
+                b = (0.055643f * x + -0.204026f * y + 1.057229f * z) * 1.1003f;
+                
+                HE_UINT32 valRet = 0xFF000000;
+				HE_UINT32 tmpByte1 = sqrtf (fz_clamp( r, 0, 1)) * 255;
+				HE_UINT32 tmpByte2 = sqrtf (fz_clamp( g, 0, 1)) * 255;
+				HE_UINT32 tmpByte3 = sqrtf (fz_clamp( b, 0, 1)) * 255;
+                valRet = valRet | tmpByte1 << 16 | tmpByte2 << 8 | tmpByte3;
+            }
+            break;
+        }
+    case COLORSPACE_CIEBASE_ICCBASED:
+        {
+            if ( mpObj && mpObj->GetType() == OBJ_TYPE_ARRAY )
+            {
+                CHE_PDF_ArrayPtr arrayPtr = mpObj->GetArrayPtr();
+                if ( arrayPtr->GetCount() < 2 )
+                {
+                    break;
+                }
+                CHE_PDF_ObjectPtr objPtr = arrayPtr->GetElement( 1, OBJ_TYPE_STREAM );
+                if ( !objPtr )
+                {
+                    break;
+                }
+                CHE_PDF_StreamPtr stmPtr = objPtr->GetStreamPtr();
+                CHE_PDF_DictionaryPtr dictPtr = stmPtr->GetDictPtr();
+                if ( !dictPtr )
+                {
+                    break;
+                }
+                objPtr = dictPtr->GetElement( "N", OBJ_TYPE_NUMBER );
+                if ( !objPtr ) {
+                    break;
+                }
+                switch ( objPtr->GetNumberPtr()->GetInteger() )
+                {
+                case 1:
+                    {
+                        HE_UINT32 valRet = 0xFF000000;
+                        HE_UINT32 tmpByte = 0x00;
+                        if ( color.mConponents.size() >= 1 )
+                        {
+                            tmpByte = color.mConponents[0] * 255;
+                            valRet = valRet | tmpByte | tmpByte << 8 | tmpByte << 16;
+                        }
+                        return valRet;
+                    }
+                case 3:
+                    {
+                        HE_UINT32 valRet = 0xFF000000;
+                        HE_UINT32 tmpByte1 = 0x00;
+                        HE_UINT32 tmpByte2 = 0x00;
+                        HE_UINT32 tmpByte3 = 0x00;
+                        if ( color.mConponents.size() >= 3 )
+                        {
+                            tmpByte1 = color.mConponents[0] * 255;
+                            tmpByte2 = color.mConponents[1] * 255;
+                            tmpByte3 = color.mConponents[2] * 255;
+                            valRet = valRet | tmpByte1 << 16 | tmpByte2 << 8 | tmpByte3;
+                        }
+                        return valRet;
+                    }
+                case 4:
+                    {
+                        if ( color.mConponents.size() >= 4 )
+                        {
+                            HE_FLOAT bgr[3];
+                            bgr[0] = ( 1 < color.mConponents[2] + color.mConponents[3] ) ? color.mConponents[2] + color.mConponents[3] : 1;
+                            bgr[1] = ( 1 < color.mConponents[1] + color.mConponents[3] ) ? color.mConponents[1] + color.mConponents[3] : 1;
+                            bgr[2] = ( 1 < color.mConponents[0] + color.mConponents[3] ) ? color.mConponents[0] + color.mConponents[3] : 1;
+                            
+                            HE_UINT32 valRet = 0xFF000000;
+                            HE_UINT32 tmpByte1 = bgr[0] * 255;
+                            HE_UINT32 tmpByte2 = bgr[1] * 255;
+                            HE_UINT32 tmpByte3 = bgr[2] * 255;
+                            valRet = valRet | tmpByte1 << 16 | tmpByte2 << 8 | tmpByte3;
+                            return valRet;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+                }
+            }
+            break;
+        }
+    case COLORSPACE_SPECIAL_INDEXED:
+        {
+            break;
+        }
+    case COLORSPACE_SPECIAL_PATTERN:
 	default:
-		return 0xFF000000;
+        break;
 	}
+    return 0xFF000000;
 }
 
 CHE_PDF_ColorSpace * GetColorSpace( const CHE_ByteString & name, CHE_Allocator * pAllocator )

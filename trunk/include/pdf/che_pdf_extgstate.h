8dcebbe1cd6e7e7a59dc5328235c32681d66b222
zencher <zencher@5bab769a-80ee-f445-9178-c608493bda25>
2014-03-24 03:17:11 +0000

    解析extgstate字典
    git-svn-id: https://10.211.55.3/svn/chelib@799 5bab769a-80ee-f445-9178-c608493bda25

--- trunk/include/pdf/che_pdf_extgstate.h
+++ trunk/include/pdf/che_pdf_extgstate.h
@@ -1,9 +1,47 @@
 #ifndef _CHE_PDF_EXTGSTATE_H_
 #define _CHE_PDF_EXTGSTATE_H_
 
+#include <vector>
 
 #include "che_pdf_component.h"
 
+
+enum PDF_EXTGSTATE_TYPE
+{
+	PDF_EXTGSTATE_LW,
+	PDF_EXTGSTATE_LJ,
+	PDF_EXTGSTATE_LC,
+	PDF_EXTGSTATE_ML,
+	PDF_EXTGSTATE_D,
+	PDF_EXTGSTATE_RI,
+	PDF_EXTGSTATE_OP,
+	PDF_EXTGSTATE_op,
+	PDF_EXTGSTATE_OPM,
+	PDF_EXTGSTATE_Font,
+	PDF_EXTGSTATE_BG,
+	PDF_EXTGSTATE_BG2,
+	PDF_EXTGSTATE_UCR,
+	PDF_EXTGSTATE_UCR2,
+	PDF_EXTGSTATE_TR,
+	PDF_EXTGSTATE_TR2,
+	PDF_EXTGSTATE_HT,
+	PDF_EXTGSTATE_FL,
+	PDF_EXTGSTATE_SM,
+	PDF_EXTGSTATE_SA,
+	PDF_EXTGSTATE_BM,
+	PDF_EXTGSTATE_SMask,
+	PDF_EXTGSTATE_CA,
+	PDF_EXTGSTATE_ca,
+	PDF_EXTGSTATE_AIS,
+	PDF_EXTGSTATE_TK
+};
+
+class CHE_PDF_ExtGStateItem
+{
+public:
+	PDF_EXTGSTATE_TYPE	type;
+	CHE_PDF_ObjectPtr	objPtr;
+};
 
 class CHE_PDF_ExtGState;
 
@@ -17,6 +55,8 @@ class CHE_PDF_ExtGState : protected CHE_PDF_Component
 {
 public:
 	static CHE_PDF_ExtGStatePtr Create( const CHE_PDF_ObjectPtr & objPtr, CHE_Allocator * pAllocator = NULL );
+
+	std::vector<CHE_PDF_ExtGStateItem> mStateItems;
 
 private:
 	CHE_PDF_ExtGState( const CHE_PDF_ObjectPtr & objPtr, CHE_Allocator * pAllocator = NULL );
--- trunk/src/pdf/che_pdf_extgstate.cpp
+++ trunk/src/pdf/che_pdf_extgstate.cpp
@@ -10,41 +10,252 @@ CHE_PDF_ExtGStatePtr CHE_PDF_ExtGState::Create( const CHE_PDF_ObjectPtr & objPtr
 		{
 			pAllocator = GetDefaultAllocator();
 		}
-		ptr.Reset( pAllocator->New<CHE_PDF_ExtGState>( objPtr, pAllocator ) );
+
+		if ( objPtr->GetType() == OBJ_TYPE_REFERENCE  )
+		{
+			CHE_PDF_ObjectPtr tmpObjPtr = objPtr->GetRefPtr()->GetRefObj( OBJ_TYPE_DICTIONARY );
+			if ( tmpObjPtr )
+			{
+				ptr.Reset( pAllocator->New<CHE_PDF_ExtGState>( tmpObjPtr, pAllocator ) );
+			}
+		}else if ( objPtr->GetType() == OBJ_TYPE_DICTIONARY )
+		{
+			ptr.Reset( pAllocator->New<CHE_PDF_ExtGState>( objPtr, pAllocator ) );
+		}
 	}
 	return ptr;
 }
 
 
-CHE_PDF_ExtGState::CHE_PDF_ExtGState( const CHE_PDF_ObjectPtr & objPtr, CHE_Allocator * pAllocator /*= NULL*/ )
-	:CHE_PDF_Component( COMPONENT_TYPE_ExtGState, objPtr, pAllocator )/*, mRootObjPtr( objPtr )*/
+CHE_PDF_ExtGState::CHE_PDF_ExtGState( const CHE_PDF_ObjectPtr & rootObjPtr, CHE_Allocator * pAllocator /*= NULL*/ )
+	:CHE_PDF_Component( COMPONENT_TYPE_ExtGState, rootObjPtr, pAllocator )
 {
-	//a lot of work todo!
+	CHE_PDF_ObjectPtr objPtr;
+	CHE_PDF_DictionaryPtr dictPtr = mRootObjPtr->GetDictPtr();
 
-	//LW
-	//LJ
-	//LC
-	//ML
-	//D
-	//RI
-	//OP
-	//op
-	//OPM
-	//Font
-	//BG
-	//BG2
-	//UCR
-	//UCR2
-	//TR
-	//TR2
-	//HT
-	//FL
-	//SM
-	//SA
-	//BM
-	//SMask
-	//CA
-	//ca
-	//AIS
-	//TK
+	CHE_PDF_ExtGStateItem item;
+
+	objPtr = dictPtr->GetElement( "LW", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_LW;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "LJ", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_LJ;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "LC", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_LC;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "ML", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_ML;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "D", OBJ_TYPE_ARRAY );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_D;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "RI", OBJ_TYPE_NAME );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_RI;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "OP", OBJ_TYPE_BOOLEAN );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_OP;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "op", OBJ_TYPE_BOOLEAN );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_op;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "OPM", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_OPM;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "Font", OBJ_TYPE_ARRAY );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_OP;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "BG" );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_BG;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "BG2" );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_BG2;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "UCR" );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_UCR;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "UCR2" );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_UCR2;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "TR" );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_TR;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "TR2" );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_TR2;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "HT" );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_HT;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "FL", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_FL;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "SM", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_SM;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+	
+	objPtr = dictPtr->GetElement( "SM", OBJ_TYPE_BOOLEAN );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_SA;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+	
+	objPtr = dictPtr->GetElement( "BM", OBJ_TYPE_NAME );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_BM;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}else{
+		objPtr = dictPtr->GetElement( "BM", OBJ_TYPE_ARRAY );
+		if ( objPtr )
+		{
+			item.type = PDF_EXTGSTATE_BM;
+			item.objPtr = objPtr;
+			mStateItems.push_back( item );
+		}
+	}
+
+	objPtr = dictPtr->GetElement( "SMask", OBJ_TYPE_NAME );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_SMask;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}else{
+		objPtr = dictPtr->GetElement( "SMask", OBJ_TYPE_DICTIONARY );
+		if ( objPtr )
+		{
+			item.type = PDF_EXTGSTATE_SMask;
+			item.objPtr = objPtr;
+			mStateItems.push_back( item );
+		}
+	}
+
+	objPtr = dictPtr->GetElement( "CA", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_CA;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "ca", OBJ_TYPE_NUMBER );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_CA;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "AIS", OBJ_TYPE_BOOLEAN );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_AIS;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
+
+	objPtr = dictPtr->GetElement( "TK", OBJ_TYPE_BOOLEAN );
+	if ( objPtr )
+	{
+		item.type = PDF_EXTGSTATE_TK;
+		item.objPtr = objPtr;
+		mStateItems.push_back( item );
+	}
 }

68882e8664b7fc7aa7ed22a925e69027440bcb21
zencher <zencher@zencherdeMacBook-Pro.local>
2016-02-02 00:45:24 +0800

    pattern单页测试修改

--- trunk/projects/xcode/chelibTests/pattern.mm
+++ trunk/projects/xcode/chelibTests/pattern.mm
@@ -8,6 +8,14 @@
 
 #import <XCTest/XCTest.h>
 
+#include "che_pdf_file.h"
+#include "che_pdf_pattern.h"
+#include "che_pdf_colorspace.h"
+#include "che_pdf_componentmgr.h"
+#include "Che_pdf_colorspace.h"
+#include "che_pdf_contentlist.h"
+#include "che_pdf_gstate.h"
+
 @interface pattern : XCTestCase
 
 @end
@@ -24,9 +32,40 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)test000 {
+    IHE_Read * pRead = HE_CreateFileRead( "/Users/zencher/testfiles/patternUnitTest000.pdf" );
+    CHE_PDF_File file;
+    file.Open( pRead );
+
+    CHE_PDF_ComponentMgr mgr;
+    CHE_PDF_ColorSpacePtr cs;
+    
+    CHE_PDF_ReferencePtr refPtr = CHE_PDF_Reference::Create( 40, 0, &file);
+    
+    bool bFound = false;
+    CHE_PDF_TilingPtr tiling = CHE_PDF_Tiling::Create( refPtr, &mgr);
+    if ( tiling )
+    {
+        CHE_PDF_ContentObjectList & content = tiling->GetList();
+        
+        ContentObjectList::iterator it;
+        for ( it = content.Begin(); it != content.End(); ++it )
+        {
+            CHE_PDF_GState * pState = (*it)->GetGState();
+            pState->GetFillColorSpace( cs );
+            if ( cs->GetColorSpaceType() == COLORSPACE_CIEBASE_ICCBASED )
+            {
+                CHE_PDF_CS_ICCBasedPtr icc = cs->GetICCBasedPtr();
+                if ( icc->mAlternate->GetColorSpaceType() == COLORSPACE_DEVICE_RGB )
+                {
+                    bFound = true;
+                }
+            }
+        }
+
+    }
+    
+    XCTAssertTrue( bFound, @"!!!" );
 }
 
 - (void)testPerformanceExample {

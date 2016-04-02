550e05cbb4ec8cecfb8516c55850e68a955ffdc5
zencher <zencher@zencherdeMacBook-Pro.local>
2016-04-02 16:12:55 +0800

    绘制时候的页面矩形应该从MediaBox来取的

--- trunk/src/pdf/che_pdf_pages.cpp
+++ trunk/src/pdf/che_pdf_pages.cpp
@@ -296,7 +296,7 @@ CHE_Rect CHE_PDF_Page::GetMediaBox() const
 CHE_Rect CHE_PDF_Page::GetPageRect() const
 {
 	CHE_Rect rect;
-	CHE_PDF_ArrayPtr arrayPtr = GetArtBoxArray();
+    CHE_PDF_ArrayPtr arrayPtr = GetMediaBoxArray();//GetArtBoxArray();
 	arrayPtr->GetRect( rect );
 	return rect;
 }
